a7d8564beae3ace939a392b6e4f5e373bb780197
Dcode24 <d.ogunseye7@gmail.com>
2023-04-28 21:01:46 +0100

    minimum Size Subarry Sum

---
+++ minSizeSubarraySum.js
@@ -0,0 +1,14 @@
+function minSubArrayLen(target, nums) {
+  let result = Number.MAX_SAFE_INTEGER;
+  let left = 0;
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    while (sum >= target) {
+      result = Math.min(result, i - left + 1);
+      sum -= nums[left++];
+    }
+  }
+  return result === Number.MAX_SAFE_INTEGER ? 0 : result;
+}
