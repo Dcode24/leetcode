280551dc390f48e96b3fbc66ecdb649aa4e1d5b8
Dcode24 <d.ogunseye7@gmail.com>
2023-05-04 23:25:24 +0100

    longest palidromic substring

---
+++ medium/longestPalidromic.js
@@ -0,0 +1,27 @@
+function longestPalindrome(s) {
+  if (s.length < 1 || s === null) return "";
+  let longest = "";
+
+  for (let i = 0; i < s.length; i++) {
+    let oddPalindrome = expandFromCenter(s, i, i);
+    let evenPalindrome = expandFromCenter(s, i - 1, i);
+
+    if (oddPalindrome.length > longest.length) {
+      longest = oddPalindrome;
+    }
+
+    if (evenPalindrome.length > longest.length) {
+      longest = evenPalindrome;
+    }
+  }
+  return longest;
+
+  function expandFromCenter(str, left, right) {
+    let i = 0;
+    while (str[left - i] && str[left - i] === str[right + i]) {
+      i++;
+    }
+    i--;
+    return str.slice(left - i, right + i + 1);
+  }
+}
